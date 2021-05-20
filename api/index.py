c406d45cc1739f4710154f61961eac1d320da01a
ruhit3 <ruhit1011@gmail.com>
2021-05-21 01:09:36 +0600

    Add flask server

--- api/index.py
+++ api/index.py
@@ -1,19 +1,9 @@
-from http.server import BaseHTTPRequestHandler
-from urllib import parse
+from flask import Flask, Response
+app = Flask(__name__)
 
 
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        s = self.path
-        dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-
-        if "name" in dic:
-            message = "Hello, " + dic["name"] + "!"
-        else:
-            message = "Hello, stranger!"
-
-        self.wfile.write(message.encode())
-        return
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path),
+                    mimetype="text/html")
